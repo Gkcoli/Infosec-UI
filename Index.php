fbaf97060c07964c63c58527a2572e4109d77386
AiahyabU <111488754+Gkcoli@users.noreply.github.com>
2024-12-30 21:05:07 +0800

    Di same sa live server T_T

---
+++ Index.php
@@ -0,0 +1,31 @@
+      <div id="payroll-section" class="payroll-section">
+            <div class="payroll-container">
+                <div class="payroll-overview">
+                    <h2 style="font-size: 40px; font-weight: bold;">Payroll Overview</h2>
+                    <h3>Upcoming Payroll Release: <span style="color: #4DA1A9; font-weight: bold;">December 29, 2024</span></h3>
+                    <div class="breakdown">
+                        <h3 style="background-color: #2E5077; color: #F6F4F0; padding: 10px; border-radius: 5px; font-size: 18px;">EXPECTED BREAKDOWN OF PAYSLIP</h3>
+                        <table>
+                            <tr>
+                                <td style="text-align: left;">Hours worked</td>
+                                <td>24 hrs</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: left;">Pay per hour</td>
+                                <td>P300.00/hr</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: left;">Deductions:</td>
+                                <td style="text-align: left;">-Taxes (10%)</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td style="color: #4DA1A9;">P30.00</td>
+                            </tr>
+                            <tr class="total">
+                                <td style="text-align: left; font-style: italic;">TOTAL AMOUNT:</td>
+                                <td style="font-style: italic">P770.00</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
